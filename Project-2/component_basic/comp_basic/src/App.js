47d03f2d1897d6db44520008ca423068eb90db1c
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-20 22:51:36 +0530

    calling functions in components
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

---
+++ Project-2/component_basic/comp_basic/src/App.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+constructor(props) {
+  super(props);
+
+  this.yourName = "Rajesh";
+  this.state = {};
+}
+
+
+  sayHello(name) {
+    return "Hello " + name;
+  }
+render(){
+  const myName = "Puneet";
+  return(
+    <div className="App">
+      <div>
+        <h2>Sample Data : {this.sayHello("Puneet")} </h2>
+        <h2>Sample Data : {this.yourName} </h2>
+      </div>
+    </div>
+  );
+}
+}
+export default App;
