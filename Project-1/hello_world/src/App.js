92a39ca6dc7dcd9cf74cc6e88837a2374bca1323
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-20 20:41:40 +0530

    importing components from different files
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

--- Project-1/hello_world/src/App.js
+++ Project-1/hello_world/src/App.js
@@ -1,61 +1,29 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Intro from './Intro';
+import Ptag from './Ptag';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+      
+      <Ptag />
       <Header />
+      <Intro />
+      
     </div>
     );
   }
 }
 
-class  Header extends Component {
-  render(){
-    return(
-      <div>
-      <Intro /> 
-         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Ptag />
-      </header>
-      </div>
-    );
-  }
-}
-
-class Ptag extends Component {
-  render(){
-    return(
-      <div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
-    );
-  }
-}
 
 
-class Intro extends Component {
-  render(){
-    return(
-      <div>
-        <h1> My First Component</h1>
-      </div>
-    );
-  }
-}
+  
+
+  
 
 
 export default App;
---
+++ Project-1/hello_world/src/Header.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+
+
+
+export default class  Header extends Component {
+    render(){
+      return(
+        <div>
+        
+           <header className="App-header">
+          {/* <img src={} className="App-logo" alt="logo" /> */}
+         
+        </header>
+        </div>
+      );
+    }
+  }
+
+  // export default Header;
