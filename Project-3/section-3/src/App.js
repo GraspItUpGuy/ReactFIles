ebc16b046f99dcbc623df33c273ab6e73ac00ac3
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-21 14:20:38 +0530

    sss
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

--- Project-3/section-3/src/App.js
+++ Project-3/section-3/src/App.js
@@ -11,18 +11,51 @@ class App extends Component{
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Parent />
       </header>
     </div>
     );
   }
 }
+
+App.propTypes = { 
+  propObject : React.propTypes.object 
+}
+
+
+class Parent extends Component{
+  render(){
+    return(
+      <div>
+        <h2>I am from Parent Component</h2>
+        <Cars msg = "Cars are really really cool !!!" model ="12321" coolcars = {this.props.cars} modelNo = {this.props.model} />
+        
+      </div>
+    );
+  }
+}
+
+Parent.defaultProps = { 
+  cars : [ "santro", "alto", "city", "i10"],
+  model : 2010,
+}
+
+
+class Cars extends Component{
+  render(){
+    console.log(this.props);
+    return(
+      <div>
+        <h3>I am from Cars Component</h3>
+        <p>{this.props.msg}</p>
+        <p>{this.props.model}</p>
+        <p>{this.props.coolcars.map((item,i) =>{
+          return i + " " + item + " " ;
+        })}</p>
+      </div>
+    );
+  }
+}
+
 
 export default App;
